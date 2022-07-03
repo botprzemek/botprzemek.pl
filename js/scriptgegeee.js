45ca507684c142610d267b034252bbc178083cd2
botprzemek <92102878+botprzemek@users.noreply.github.com>
2022-07-03 10:42:57 +0200

    Fixxxx

--- js/scriptgegeee.js
+++ js/scriptgegeee.js
@@ -123,7 +123,7 @@ function waiting() {
   let lastSection = localStorage.getItem("lastSection");
   if (lastSection != 0) {
     lastSection--;
-  } else if (lastSection == 0) {
+  } else if (lastSection == 0 || lastSection == -1) {
     lastSection = 1;
   }
   console.log(lastSection);
