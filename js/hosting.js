83fd5e1260ff065ea5722ae47ccc4482ea052724
botprzemek <92102878+botprzemek@users.noreply.github.com>
2022-07-06 07:05:12 +0200

    First section (Hosting)

---
+++ js/hosting.js
@@ -0,0 +1,143 @@
+let containers = new Array();
+let columns = new Array();
+let columnsText = new Array();
+let rows = new Array();
+let rowsText = new Array();
+let textArray = new Array();
+let tabSize = 4;
+textArray[0] = ["Mini", "Medium", "Pro", "Expert"];
+textArray[1] = ["4GB RAM", "8GB RAM", "16GB RAM", "24GB RAM"];
+textArray[2] = ["Do 10 Graczy", "Do 20 Graczy", "Do 30 Graczy", "Do 40 Graczy"];
+textArray[3] = ["Dysk 2GB", "Dysk 4GB", "Dysk 8GB", "Dysk +16GB"];
+textArray[4] = [
+  "Brak pluginów",
+  "Do 10 Pluginów",
+  "Do 15 Pluginów",
+  "Do 30+ Pluginów",
+];
+textArray[5] = [
+  "Wersja 1.8-1.19",
+  "Wersja 1.8-1.19",
+  "Wersja 1.8-1.19",
+  "Wersja 1.8-1.19",
+];
+textArray[6] = [
+  "Vanilla <br /> Minecraft",
+  "Paper/Purpur <br /> Minecraft",
+  "Paper/Purpur <br /> Minecraft",
+  "Paper/Purpur/Forge <br /> Minecraft",
+];
+textArray[7] = ["/", "/", "/", "/"];
+function createElements(elementSize) {
+  let wait = 0;
+  setTimeout(() => {
+    wait = 1;
+    hoverContainer();
+  }, 700);
+  function cancel(e) {
+    if (wait == 0) e.preventDefault();
+  }
+  document.body.addEventListener("click", cancel);
+  window.addEventListener("contextmenu", cancel);
+  let body = document.createElement("div");
+  let loadBackground = document.createElement("div");
+  loadBackground.classList.add("loadBackground");
+  let tableTitle = document.createElement("p");
+  let table = document.createElement("div");
+  tableTitle.textContent = "Hosting";
+  body.classList.add("body");
+  tableTitle.classList.add("title");
+  table.classList.add("table");
+  table.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${elementSize}, 1fr)`
+  );
+  document.body.appendChild(body);
+  body.appendChild(loadBackground);
+  body.appendChild(tableTitle);
+  body.appendChild(table);
+  setTimeout(() => {
+    loadBackground.style.transform = "translateY(-100%)";
+  }, 1);
+  for (let i = 0; i < elementSize; i++) {
+    containers[i] = document.createElement("div");
+    containers[i].classList.add("container");
+    containers[i].setAttribute("link", textArray[7][i]);
+    columns[i] = document.createElement("div");
+    columns[i].classList.add("box", "column");
+    columnsText[i] = document.createElement("h1");
+    columnsText[i].textContent = textArray[0][i];
+    columns[i].appendChild(columnsText[i]);
+    containers[i].appendChild(columns[i]);
+    for (let j = 0; j < 6; j++) {
+      rows[j] = document.createElement("div");
+      rows[j].classList.add("box", "row");
+      rowsText[j] = document.createElement("p");
+      if (textArray[j + 1] != undefined)
+        rowsText[j].innerHTML = textArray[j + 1][i];
+      else rowsText[j].textContent = "Brak danych";
+      rows[j].appendChild(rowsText[j]);
+      containers[i].appendChild(rows[j]);
+    }
+    table.appendChild(containers[i]);
+  }
+  function animateElements(amount, elements) {
+    for (let i = 0; i < amount; i++) {
+      elements[i].style.transform = "translateY(12%)";
+      elements[i].style.opacity = "0";
+      setTimeout(() => {
+        elements[i].style.transform = "translateY(0%)";
+        elements[i].style.opacity = "1";
+      }, (i + 1.3) * 200 + 200);
+    }
+  }
+  function animateElement(element, time) {
+    element.style.transform = "translateY(12%)";
+    element.style.opacity = "0";
+    setTimeout(() => {
+      element.style.transform = "translateY(0%)";
+      element.style.opacity = "1";
+    }, time);
+  }
+  function hoverContainer() {
+    containers.forEach((container) => {
+      container.addEventListener("mouseover", () => {
+        container.style.transform = "translateY(-8%)";
+        container.style.cursor = "pointer";
+      });
+      container.addEventListener("mouseout", (e) => {
+        container.style.transform = "translateY(0%)";
+        container.style.cursor = "auto";
+      });
+      container.addEventListener("click", (e) => {
+        if (document.querySelector(".nextPage") != undefined)
+          document.body.removeChild(document.querySelector(".nextPage"));
+        let nextPage = document.createElement("div");
+        let size = 100;
+        nextPage.classList.add("nextPage");
+        nextPage.style.width = size + "px";
+        nextPage.style.height = size + "px";
+        nextPage.style.transform = "scale(0.0001)";
+        nextPage.style.left = e.clientX - size * 0.5 + "px";
+        nextPage.style.top = e.clientY - size * 0.5 + "px";
+        document.body.appendChild(nextPage);
+        setTimeout(() => {
+          nextPage.style.transform = "scale(45)";
+        }, 1);
+        setTimeout(() => {
+          body.style.opacity = "0";
+          nextPage.style.transform = "scale(0.0001)";
+          setTimeout(() => {
+            window.location.href = container.getAttribute("link");
+            setTimeout(() => {
+              body.style.opacity = "1";
+            }, 1700);
+          }, 1322);
+        }, 1111);
+      });
+    });
+  }
+  animateElement(tableTitle, 130);
+  animateElements(tabSize, containers);
+}
+window.onload = createElements(tabSize);
---
+++ js/main.js
@@ -0,0 +1,220 @@
+const navBar = document.querySelector(".navbar");
+const indicator = document.querySelector(".indicator");
+let allLi = document.querySelectorAll("li");
+let wait = 0;
+let h = innerHeight;
+let sectionOf = new Array();
+let boxedSections = new Array();
+let boxedBox = new Array();
+let boxedText = new Array();
+let backgroundSections = new Array();
+let backgroundTexts = new Array();
+let amount = 2;
+let lang = new Array();
+let categoryText = new Array();
+let categoryLinks = new Array();
+lang["en"] = ["welcome", "my projects"];
+lang["pl"] = ["witaj", "moje projekty", "wgerrweg wreg"];
+categoryText[0] = ["bpPanel", "hosting"];
+categoryLinks[0] = ["../", "hosting.html"];
+categoryText[1] = ["test1", "test2"];
+categoryLinks[1] = ["../", "../"];
+
+// SECTIONS
+
+function sections() {
+  for (let i = 0; i < amount; i++) {
+    sectionOf[i] = document.createElement("div");
+    sectionOf[i].classList.add("section");
+    sectionOf[i].id = "s" + (i + 1);
+    boxedSections[i] = document.createElement("div");
+    boxedSections[i].classList.add("boxed");
+    backgroundSections[i] = document.createElement("div");
+    backgroundSections[i].classList.add("background");
+    backgroundTexts[i] = document.createElement("h1");
+    backgroundTexts[i].classList.add("text", "front");
+    backgroundTexts[i].style.display = "none";
+    setTimeout(() => {
+      backgroundTexts[i].style.display = "inline";
+    }, 250);
+    if (window.navigator.language == "pl" && lang["pl"][i] != undefined) {
+      backgroundTexts[i].textContent = lang["pl"][i];
+      if (lang["pl"][i] == undefined) backgroundTexts[i].textContent = "null";
+    }
+    if (window.navigator.language != "pl" && lang["en"][i] != undefined) {
+      backgroundTexts[i].textContent = lang["en"][i];
+      if (lang["en"][i] == undefined) backgroundTexts[i].textContent = "null";
+    }
+    backgroundSections[i].appendChild(backgroundTexts[i]);
+    sectionOf[i].appendChild(boxedSections[i]);
+    sectionOf[i].appendChild(backgroundSections[i]);
+    for (let j = 0; j < 2; j++) {
+      boxedBox[j] = document.createElement("div");
+      boxedBox[j].classList.add("test");
+      boxedBox[j].setAttribute("link", categoryLinks[i][j]);
+      setTimeout(() => {
+        boxedBox[j].style.display = "flex";
+      }, 250);
+      if (j == 0) boxedBox[j].classList.add("left");
+      else if (j == 1) boxedBox[j].classList.add("right");
+      boxedText[j] = document.createElement("h1");
+      if (categoryText[i] != undefined)
+        boxedText[j].textContent = categoryText[i][j];
+      else boxedText[j].textContent = "null";
+      boxedSections[i].appendChild(boxedBox[j]);
+      boxedBox[j].appendChild(boxedText[j]);
+    }
+    document.getElementById("content").appendChild(sectionOf[i]);
+  }
+}
+
+sections();
+let first_id = "s1";
+let last_id = ("s" + sectionOf.length).toString();
+
+function waiting() {
+  document.body.style.opacity = "0";
+  setTimeout(() => {
+    document.body.style.opacity = "1";
+  }, 10);
+  navBar.querySelector(".active").classList.remove("active");
+  let lastSection = localStorage.getItem("lastSection");
+  if (lastSection <= 0 || lastSection === undefined) {
+    allLi[0].classList.add("active");
+    indicator.style.transform = `translateX(0px)`;
+  } else {
+    allLi[lastSection - 1].classList.add("active");
+    indicator.style.transform = `translateX(${(lastSection - 1) * 90}px)`;
+  }
+
+  if (wait == 0) {
+    document.body.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+    document.body.addEventListener("mouseover", (e) => {
+      e.preventDefault();
+    });
+  }
+  sectionOf.forEach((section) => {
+    section.addEventListener("wheel", (e) => {
+      if (wait == 0) e.preventDefault();
+    });
+  });
+  document.body.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+  });
+  document.body.addEventListener("keydown", (e) => {
+    if (e.key == "Backspace") e.preventDefault();
+  });
+}
+
+function operation(operation) {
+  localStorage.setItem("lastSection", operation);
+  document.getElementById("b" + operation).classList.add("active");
+  indicator.style.transform = `translateX(calc(${(operation - 1) * 90}px))`;
+}
+
+function menu() {
+  allLi.forEach((li, index) => {
+    li.addEventListener("click", (e) => {
+      e.preventDefault();
+      navBar.querySelector(".active").classList.remove("active");
+      li.classList.add("active");
+
+      localStorage.setItem("lastSection", index + 1);
+      indicator.style.transform = `translateX(calc(${index * 90}px))`;
+
+      setTimeout(() => {
+        content.focus();
+        content.scrollTo({
+          top: index * h,
+          left: 0,
+          behavior: "smooth",
+        });
+      }, 10);
+    });
+  });
+  sectionOf.forEach((section, index) => {
+    let indexed = index + 1;
+    let waitWheel = 0;
+    section.addEventListener(
+      "wheel",
+      (e) => {
+        once: true;
+        if (waitWheel == 1) {
+          e.preventDefault();
+          setTimeout(() => {
+            waitWheel = 0;
+            return;
+          }, 20);
+        } else {
+          if (
+            (e.deltaY < 0 && section.id == first_id) ||
+            (e.deltaY > 0 && section.id == last_id)
+          ) {
+            return;
+          }
+          navBar.querySelector(".active").classList.remove("active");
+          if (e.deltaY < 0) {
+            operation(indexed - 1);
+          } else if (e.deltaY > 0) {
+            operation(indexed + 1);
+          }
+          waitWheel = 1;
+          setTimeout(() => {
+            waitWheel = 0;
+          }, 20);
+        }
+      },
+      { passive: true }
+    );
+  });
+}
+
+function categories() {
+  let category = document.querySelectorAll(".test");
+  category.forEach((one) => {
+    if (one.previousElementSibling == null) two = one.nextElementSibling;
+    else two = one.previousElementSibling;
+    let click = 0;
+    one.addEventListener("mouseover", () => {
+      one.style.cursor = "pointer";
+    });
+    one.addEventListener("mouseout", () => {
+      one.style.cursor = "auto";
+    });
+    one.addEventListener("click", () => {
+      if (click == 1) {
+        one.style = "width: 25% !important; z-index: 6 !important";
+        one.parentNode.style = "height: 30% !important";
+        click--;
+      } else {
+        one.style = "width: 50% !important; z-index: 10 !important";
+        two.style = "width: 25% !important; z-index: 6 !important";
+        one.parentNode.style = "height: 100% !important";
+        one.style.cursor = "auto";
+        setTimeout(() => {
+          window.location.href = one.getAttribute("link");
+          one.style = "width: 25% !important; z-index: 6 !important";
+          one.parentNode.style = "height: 30% !important";
+        }, 1100);
+        click++;
+      }
+      sectionOf.forEach((section) => {
+        section.addEventListener("wheel", (e) => {
+          if (click == 1) e.preventDefault();
+        });
+      });
+    });
+  });
+}
+
+waiting();
+setTimeout(() => {
+  navBar.style.transform = "translateY(0%)";
+  indicator.style.transition =
+    "all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55)";
+  wait = 1;
+  menu();
+  categories();
+}, 1000);
