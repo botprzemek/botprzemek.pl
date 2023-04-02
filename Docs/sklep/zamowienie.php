0fb093024743ce267e3875b52fa4a3caabf87a0a
botprzemek <botprzemekk@gmail.com>
2023-04-02 21:40:33 +0200

    Add files via upload

---
+++ Docs/sklep/zamowienie.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zamówienie</title>
+</head>
+<body>
+    <form action="zapytanie.php">
+        <select name="typ">
+            <option value="mar">Margherita</option>
+            <option value="cap">Capricciosa</option>
+            <option value="fun">Funghi</option>
+            <option value="jal">Jalapeño</option>
+        </select>
+    </form>
+</body>
+</html>
